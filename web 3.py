83f3c5eb6d4184308c22a99f8124d29e22ec02e1
mageshwaranmug25it-source <mageshwaranm.ug.25.it@francisxavier.ac.in>
2026-07-21 23:07:03 -0700

    Add files via upload

---
+++ web 3.py
@@ -0,0 +1,26 @@
+# Enter marks for five subjects
+m1 = float(input("Enter marks of Subject 1: "))
+m2 = float(input("Enter marks of Subject 2: "))
+m3 = float(input("Enter marks of Subject 3: "))
+m4 = float(input("Enter marks of Subject 4: "))
+m5 = float(input("Enter marks of Subject 5: "))
+
+total = m1 + m2 + m3 + m4 + m5
+average = total / 5
+
+if average >= 90:
+    grade = "A+"
+elif average >= 80:
+    grade = "A"
+elif average >= 70:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Total =", total)
+print("Average =", average)
+print("Grade =", grade)
